d8156e8e95cd0bdd6a3ac29f742a14a4fd8328eb
Alexander Gritsok <agashayasha@gmail.com>
2020-05-02 13:14:39 +0300

    Added log4j2 logger with config .xml file

---
+++ src/main/java/logger/Log.java
@@ -0,0 +1,24 @@
+package logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log {
+
+    private static final Logger LOGGER = LogManager.getLogger("com.wastelogics");
+
+    private Log() {
+    }
+
+    public static void debug(String message) {
+        LOGGER.debug(message);
+    }
+
+    public static void info(String message) {
+        LOGGER.info(message);
+    }
+
+    public static void error(String message) {
+        LOGGER.error(message);
+    }
+}
